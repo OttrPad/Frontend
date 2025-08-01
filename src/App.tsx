f5595abc20bee20837c925ded96616df69b66ab0
Mandira Dewindi <dewindigm.23@uom.lk>
2025-08-01 13:58:26 +0000

    Set up React Router with login at root and rooms at /join
    cgen-5e24708ae908489e96ea6005349e89c9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/LoginPage";
 import RoomsPage from "./pages/rooms/RoomsPage";
 
 function App() {
   return (
-    <>
-      <RoomsPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/join" element={<RoomsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
